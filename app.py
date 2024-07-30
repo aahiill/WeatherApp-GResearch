aad708bc3614040d9d92734444751171f3280e57
Aahil <aahilnamajee@gmail.com>
2024-07-30 15:55:27 +0100

    Added weather.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
 app = Flask(__name__)
 @app.route('/')
 
