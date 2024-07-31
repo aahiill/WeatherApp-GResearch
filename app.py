0468af2f3c7694c9e49170c83ecd0e59be6a4e34
Mirsaid <abdullaevmirsaid005@gmail.com>
2024-07-31 10:02:24 +0100

    Fixed forecast.html

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 LATITUDE = 52.52  # Example: Berlin
 LONGITUDE = 13.405
 
+
 @app.route('/')
+
+
 def home():
     # OpenMeteo API endpoint
     url = f"https://api.open-meteo.com/v1/forecast?latitude={LATITUDE}&longitude={LONGITUDE}&daily=temperature_2m_max,temperature_2m_min&timezone=Europe/Berlin"
@@ -26,6 +29,7 @@ def home():
     weekly_forecast = zip(dates, temp_max, temp_min)
 
     return render_template('forecast.html', weekly_forecast=weekly_forecast, LATITUDE=LATITUDE, LONGITUDE=LONGITUDE)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
